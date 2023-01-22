4ae37fc1feb94e8adb05c761247f50aa4fe91243
Sedakat <sedakatekmekci16@gmail.com>
2023-01-22 12:30:26 +0300

    update changes

---
+++ todo-app/src/components/Todos/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todos() {
+    return (
+        <div className='todos'>
+            <div className='header'>
+                <h1>Todos</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Todos;

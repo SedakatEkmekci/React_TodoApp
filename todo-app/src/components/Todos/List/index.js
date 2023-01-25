7a754b68345e09c45b64f34852b798f8c5be97cc
Sedakat <sedakatekmekci16@gmail.com>
2023-01-25 14:53:46 +0300

    update

--- todo-app/src/components/Todos/List/index.js
+++ todo-app/src/components/Todos/List/index.js
@@ -3,10 +3,8 @@ import React, { useState } from 'react';
 
 function List({ tasks }) {
 
-    const handleOnChecked = () => {
 
 
-    }
     return (
         <section className="main">
             <input className="toggle-all" type="checkbox" />
@@ -17,12 +15,17 @@ function List({ tasks }) {
             <ul className="todo-list">
                 {
                     tasks.map((tasks, i) => (
-                        <li className="completed" key={i}>
+
+                        <li className="completed" key={i} >
                             <div className="view">
                                 <span>{tasks.taskTitle}</span>
                                 <input
+
+                                    name="taskStatus"
+                                    value={tasks.taskStatus}
                                     className="toggle"
                                     type="checkbox"
+
                                 />
                             </div>
                         </li>
